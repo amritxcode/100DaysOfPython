231fb59531c2fab5c85685c6f4dc4e280b6a6a31
Amrit Ray <amritray43@gmail.com>
2026-03-28 14:32:15 +0530

    Restore missing files

---
+++ Personal_Projects/Weather_App/README.md
@@ -0,0 +1,18 @@
+# Weather App
+
+A simple Python CLI app that fetches real-time weather using OpenWeather API.
+
+## Features
+- User input city
+- API integration
+- Error handling
+- Clean output
+
+## Tech Used
+- Python
+- Requests API
+
+## Learning Outcome
+- Functions
+- Exception Handling
+- API Handling
--- Personal_Projects/Weather_App/main.py
+++ Personal_Projects/Weather_App/main.py
@@ -8,18 +8,22 @@ def get_weather(city):
         url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
 
         response = requests.get(url)
-        data = (response.json())
+        if response.status_code != 200:
+            return None,None
+        data = response.json()
 
         temp = data["main"]["temp"]
         weather = data["weather"][0]["description"]
 
         return temp, weather
+    
     except:
         return None, None
     
 city = input("Enter city: ")
 
 temp, weather = get_weather(city)
+
 if temp is None:
     print("Something went wrong. Check city name or internet.")
 else:
